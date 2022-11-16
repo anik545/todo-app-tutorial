465c4068b85db2b10d729d55d28e414240661b58
anik545 <anik545@gmail.com>
2022-11-16 16:28:07 +0000

    .

--- App.tsx
+++ App.tsx
@@ -11,9 +11,15 @@ interface Todo {
 export const App: React.FC<{}> = () => {
   const [todos, setTodos] = React.useState<Todo[]>([]);
   const [newTodoTitle, setNewTodoTitle] = React.useState('');
+  const [showDoneTodos, setShowDoneTodos] = React.useState(false);
+
+  const toggleShowDoneTodos = () => setShowDoneTodos((x) => !x);
 
   const addNewTodo = () =>
     setTodos([...todos, { id: uuid(), title: newTodoTitle }]);
+
+  const onChangeTodoWithId = (id: number) => (partial: Partial<Todo>) =>
+    setTodos(todos.map((x) => (x.id === id ? { ...x, ...partial } : x)));
 
   return (
     <div className={css.container}>
@@ -23,9 +29,34 @@ export const App: React.FC<{}> = () => {
         onChange={(e) => setNewTodoTitle(e.target.value)}
       />
       <Button onClick={addNewTodo} text="Add todo" icon="add" />
-      {todos.map((x) => (
-        <div>{x.title}</div>
-      ))}
+      <Button onClick={toggleShowDoneTodos}>
+        {showDoneTodos ? 'hide finished TODOs' : 'show finished TODOs'}
+      </Button>
+      {todos.map((todo) => {
+        if (!showDoneTodos && todo.done) {
+          return null;
+        }
+        return (
+          <SingleTodo onChangeTodo={onChangeTodoWithId(todo.id)} todo={todo} />
+        );
+      })}
+    </div>
+  );
+};
+
+export interface SingleTodoProps {
+  todo: Todo;
+  onChangeTodo(x: Partial<Todo>): void;
+}
+
+const SingleTodo: React.FC<SingleTodoProps> = ({ todo, onChangeTodo }) => {
+  return (
+    <div className={css.todo}>
+      <Checkbox
+        checked={todo.done}
+        onChange={() => onChangeTodo({ done: !todo.done })}
+      />
+      {todo.title}
     </div>
   );
 };
