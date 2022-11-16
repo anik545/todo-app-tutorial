31aa14e08808ae5553d5b1c8923efb38e2e9d613
anik545 <anik545@gmail.com>
2022-11-16 16:28:07 +0000

    .

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ interface Todo {
 export const App: React.FC<{}> = () => {
   const [todos, setTodos] = React.useState<Todo[]>([]);
   const [currentTodo, setCurrentTodo] = React.useState('');
+  const [showDoneTodos, setShowDoneTodos] = React.useState(false);
   const onChangeTodoWithId = (id: number) => (partial: Partial<Todo>) =>
     setTodos(todos.map((x) => (x.id === id ? { ...x, ...partial } : x)));
   return (
@@ -22,27 +23,40 @@ export const App: React.FC<{}> = () => {
         onChange={(e) => setCurrentTodo(e.target.value)}
       />
       <Button
-        onClick={() => setTodos([...todos, { id: uuid(), title: currentTodo, done: false }])}
+        onClick={() =>
+          setTodos([...todos, { id: uuid(), title: currentTodo, done: false }])
+        }
       >
         Add todo
       </Button>
-      {todos.map((x) => (
-        <SingleTodo onChangeTodo={onChangeTodoWithId(x.id)}/>
-      ))}
+      <Button onClick={() => setShowDoneTodos((x) => !x)}>
+        {showDoneTodos ? 'hide finished TODOs' : 'show finished TODOs'}
+      </Button>
+      {todos.map((todo) => {
+        if (!showDoneTodos && todo.done) {
+          return null;
+        }
+        return (
+          <SingleTodo onChangeTodo={onChangeTodoWithId(todo.id)} todo={todo} />
+        );
+      })}
     </div>
   );
 };
 
 export interface SingleTodoProps {
-  todo: Todo
-  onChangeTodo(x: Partial<Todo>): void
+  todo: Todo;
+  onChangeTodo(x: Partial<Todo>): void;
 }
 
-export const SingleTodo: React.FC<SingleTodoProps> = ({todo, onChangeTodo}) => {
-    return (
-      <div>
-        <Checkbox checked={todo.done} onChange={onChangeTodo({done: !todo.done})}/>
-        {todo.title}       
-      </div>
-    );
-}
+const SingleTodo: React.FC<SingleTodoProps> = ({ todo, onChangeTodo }) => {
+  return (
+    <div className={css.todo}>
+      <Checkbox
+        checked={todo.done}
+        onChange={() => onChangeTodo({ done: !todo.done })}
+      />
+      {todo.title}
+    </div>
+  );
+};
