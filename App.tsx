e0d329cf221a2552c72cbe956f83589a6ccc154e
anik545 <anik545@gmail.com>
2022-11-16 16:19:17 +0000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { InputGroup } from '@blueprintjs/core';
+import { Button, InputGroup } from '@blueprintjs/core';
 import * as React from 'react';
 import css from './style.module.scss';
+import { v4 as uuid } from 'uuid';
+
+interface Todo {
+  id: number;
+  title: string;
+}
 
 export const App: React.FC<{}> = () => {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [currentTodo, setCurrentTodo] = React.useState('');
   return (
     <div className={css.container}>
-      <h1>Hello World</h1>
+      <InputGroup
+        type="text"
+        value={currentTodo}
+        onChange={(e) => setCurrentTodo(e.target.value)}
+      />
+      <Button
+        onClick={() => setTodos([...todos, { id: uuid(), title: currentTodo }])}
+      >
+        Add todo
+      </Button>
+      {todos.map((x) => (
+        <div>{x.title}</div>
+      ))}
     </div>
   );
 };
