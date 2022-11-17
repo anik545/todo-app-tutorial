42d710c116c5bde89ca29ba89277783dfff243c9
anik545 <anik545@gmail.com>
2022-11-17 15:02:30 +0000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { InputGroup } from '@blueprintjs/core';
+import { Button, Icon, InputGroup } from '@blueprintjs/core';
 import * as React from 'react';
 import css from './style.module.scss';
 
+interface Todo {
+  id: string;
+  title: string;
+}
+
 export const App: React.FC<{}> = () => {
+  const [inputValue, setInputValue] = React.useState('');
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const onDelete = (id: string) => {
+    const newTodos = todos.filter((x) => x.id !== id);
+    setTodos(newTodos);
+  };
   return (
-    <div className={css.container}>
-      <h1>Hello World</h1>
+    <React.Fragment>
+      <div className={css.container}>
+        <h1>TODOs</h1>
+        <InputGroup
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          onClick={() =>
+            setTodos([
+              ...todos,
+              { id: Math.random().toString(), title: inputValue },
+            ])
+          }
+        >
+          Add TODO
+        </Button>
+        <div>this should be a list of todos</div>
+        {todos.map((x) => (
+          <SingleTodo todo={x} onDelete={onDelete} />
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export const SingleTodo: React.FC<{
+  todo: Todo;
+  onDelete: (id: string) => void;
+}> = ({ todo, onDelete }) => {
+  return (
+    <div>
+      {todo.title}
+      <Icon icon="trash" onClick={() => onDelete(todo.id)} />
     </div>
   );
 };
