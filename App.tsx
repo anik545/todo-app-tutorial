241f895fd77d245d71896148d59f7f106e6d6048
anik545 <anik545@gmail.com>
2023-02-19 21:40:07 +0000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,18 +2,7 @@ import { Button, Icon, InputGroup } from '@blueprintjs/core';
 import * as React from 'react';
 import css from './style.module.scss';
 
-interface Todo {
-  id: string;
-  title: string;
-}
-
 export const App: React.FC<{}> = () => {
-  const [inputValue, setInputValue] = React.useState('');
-  const [todos, setTodos] = React.useState<Todo[]>([]);
-  const onDelete = (id: string) => {
-    const newTodos = todos.filter((x) => x.id !== id);
-    setTodos(newTodos);
-  };
   return (
     <div className={css.container}>
       <h1>TODO App</h1>
