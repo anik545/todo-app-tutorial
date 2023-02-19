037aef7f91989f522f72b485255e1d7ab0638198
anik545 <anik545@gmail.com>
2023-02-19 21:39:41 +0000

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -15,40 +15,8 @@ export const App: React.FC<{}> = () => {
     setTodos(newTodos);
   };
   return (
-    <React.Fragment>
-      <div className={css.container}>
-        <h1>TODOs</h1>
-        <InputGroup
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          onClick={() =>
-            setTodos([
-              ...todos,
-              { id: Math.random().toString(), title: inputValue },
-            ])
-          }
-        >
-          Add TODO
-        </Button>
-        <div>this should be a list of todos</div>
-        {todos.map((x) => (
-          <SingleTodo todo={x} onDelete={onDelete} />
-        ))}
-      </div>
-    </React.Fragment>
-  );
-};
-
-export const SingleTodo: React.FC<{
-  todo: Todo;
-  onDelete: (id: string) => void;
-}> = ({ todo, onDelete }) => {
-  return (
-    <div>
-      {todo.title}
-      <Icon icon="trash" onClick={() => onDelete(todo.id)} />
+    <div className={css.container}>
+      <h1>TODO App</h1>
     </div>
   );
 };
